b049edaaaef9d44f956af99133075fcf7edf6350
James <admin@floretos.com>
2022-05-03 21:13:06 +0700

    black and white additions

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -409,13 +409,13 @@ export default function Example() {
                             <div>
                                 <a
                                     href="#"
-                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-gray-800 bg-gray-300 hover:bg-gray-400"
                                 >
                                     Sign up
                                 </a>
                                 <p className="mt-6 text-center text-base font-medium text-gray-500">
                                     Existing customer?{" "}
-                                    <a href="#" className="text-indigo-600 hover:text-indigo-500">
+                                    <a href="#" className="text-black hover:text-black-800">
                                         Sign in
                                     </a>
                                 </p>
