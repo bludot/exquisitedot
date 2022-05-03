f3c8ff704df1548fa9d5fb4ddee225988c3dcbdf
James <admin@floretos.com>
2022-05-03 21:02:56 +0700

    fix margin gallery, also black & white

--- pages/gallery.js
+++ pages/gallery.js
@@ -5,7 +5,7 @@ export default function GalleryPage() {
     return (
         <Layout>
             <div
-                class="container grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 mx-auto mx-16 my-32">
+                class="container content-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 mx-auto ">
                 <Image/>
                 <Image/>
                 <Image/>
